b44a080620fc6d2a15e14b0dd9c13fb95c64814e
Richard Hulse <richard.hulse@radionz.co.nz>
2016-03-30 20:40:55 +1300

    Rename object to match actual class name and add examples

--- thing_decorator.rb
+++ thing_decorator.rb
@@ -2,7 +2,7 @@ class ThingDecorator < ApplicationDecorator
 
   alias thing o
 
-  THING_PASS_THROUGH_METHODS = %w[]
+  THING_PASS_THROUGH_METHODS = %w[method_one method_two method_returns_boolean?]
 
   THING_PASS_THROUGH_METHODS.each do |method|
     define_method method do |*args|
@@ -14,4 +14,9 @@ class ThingDecorator < ApplicationDecorator
     # some code that transforms thing.another_method in some useful way
   end
   
+  private
+  
+  def an_internal_method
+    # some calculations you don't want to expose in the view
+  end
 end
